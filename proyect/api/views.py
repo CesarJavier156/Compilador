c1ec1f8a9de26c0c3112cca2e770372818056d2e
CesarJavier156 <cesarramgon05@gmail.com>
2025-11-10 10:04:28 -0600

    segundo commit

---
+++ proyect/api/views.py
@@ -0,0 +1,11 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+@api_view(['POST'])
+def main(request):
+    return Response(
+        {"mensaje":"hola"},
+        status=status.HTTP_200_OK
+    )
+
